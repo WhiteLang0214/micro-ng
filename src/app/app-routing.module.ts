cd0a42e656d45cceac073e82fbe543b2abe34d59
郎小姐 <langxue_dalang@163.com>
2023-10-17 09:16:12 +0800

    新建about页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { LoginComponent } from '../views/login/login.component';
+import { AboutComponent } from '../views/about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
